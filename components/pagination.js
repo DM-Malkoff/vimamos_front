f54f74427c5c27c2621ec6185dce382853fb0658
DMALKOV\dmalkov <demoncfh@mail.ru>
2025-06-12 18:58:04 +0400

    правка логики пагинации

--- components/pagination.js
+++ components/pagination.js
@@ -5,78 +5,80 @@ import {useRouter} from "next/router";
 const Pagination = ({totalQuantityProducts, productsLength}) => {
     const router = useRouter()
     const currentSlug = router.query.slug
-    const currentPage = router.query.page !== undefined ? +router.query.page : undefined
+    // Если page не указан, то это первая страница
+    const currentPage = router.query.page !== undefined ? +router.query.page : 1
     const {slug: _, ...routerQueries} = router.query
     const {page:__, ...routerQueriesWithoutPage} = routerQueries
 
-    const pageNumbers = []
+    // Правильно вычисляем общее количество страниц
+    const totalPages = Math.ceil(totalQuantityProducts / quantityProducts)
+    
+    // Если продуктов меньше чем на одну страницу, не показываем пагинацию
+    if (totalPages <= 1) {
+        return null
+    }
 
-    if (productsLength < quantityProducts){
-        for (let i = 1; i <= router.query.page; i++) {
-            pageNumbers.push(i)
-        }
-    }else{
-        for (let i = 1; i <= Math.ceil(totalQuantityProducts / quantityProducts); i++) {
-            pageNumbers.push(i)
-        }
+    const pageNumbers = []
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i)
     }
 
     return (
         <div className="shop_pagelist_wrap">
             <ul className="shop_pagelist">
-                <li className={`page-prev ${(currentPage == undefined || currentPage == 1) ? 'not_active' : ''}`}>
-                    <Link href={{
-                        pathname: `/catalog/${currentSlug}`,
-                        query: {
-                            ...routerQueries,
-                            page: currentPage - 1
-                        }
-                    }}
-                    ><a></a></Link></li>
+                {/* Кнопка "Предыдущая" */}
+                <li className={`page-prev ${currentPage === 1 ? 'not_active' : ''}`}>
+                    {currentPage > 1 && (
+                        <Link href={{
+                            pathname: `/catalog/${currentSlug}`,
+                            query: currentPage === 2 ? routerQueriesWithoutPage : {
+                                ...routerQueries,
+                                page: currentPage - 1
+                            }
+                        }}>
+                            <a></a>
+                        </Link>
+                    )}
+                </li>
 
-                {currentPage > 1
-                    ?
+                {/* Первая страница */}
+                {currentPage === 1 ? (
+                    <li className="page-num active-num">
+                        <span>1</span>
+                    </li>
+                ) : (
                     <li className="page-num">
                         <Link href={{
                             pathname: `/catalog/${currentSlug}`,
-                            query: {
-                                ...routerQueriesWithoutPage,
-                            }
-                        }}
-                        ><a>1</a>
-                        </Link></li>
-                    :
-                    <li className="page-num active-num"><span>1</span></li>
-                }
-                {
-                    currentPage > 3 ? <li className="page-num"><span>...</span></li> : false
-                }
-                {pageNumbers.map((number) => { // Убрали `index`, он не нужен
-                    if (currentPage == undefined && number < 5 && number > 1) {
-                        return (
-                            <li key={number} className="page-num"> {/* Используем number вместо index */}
-                                <Link href={{
-                                    pathname: `/catalog/${currentSlug}`,
-                                    query: {
-                                        ...routerQueries,
-                                        page: number
-                                    }
-                                }}>
-                                    <a>{number}</a>
-                                </Link>
-                            </li>
-                        )
-                    }
-                    if (number > 1 && (number == currentPage || number == currentPage - 1 || number == currentPage + 1)) {
-                        if (number == currentPage) {
+                            query: routerQueriesWithoutPage
+                        }}>
+                            <a>1</a>
+                        </Link>
+                    </li>
+                )}
+
+                {/* Многоточие слева */}
+                {currentPage > 3 && (
+                    <li className="page-num">
+                        <span>...</span>
+                    </li>
+                )}
+
+                {/* Страницы вокруг текущей */}
+                {pageNumbers.map((number) => {
+                    // Показываем страницы вокруг текущей (кроме первой и последней)
+                    if (number > 1 && number < totalPages && 
+                        (number === currentPage - 1 || number === currentPage || number === currentPage + 1)) {
+                        
+                        if (number === currentPage) {
                             return (
-                                <li key={number} className="page-num active-num"> {/* Ключ = number */}
+                                <li key={number} className="page-num active-num">
                                     <span>{number}</span>
                                 </li>
                             )
                         } else {
                             return (
-                                <li key={number} className="page-num"> {/* Ключ = number */}
+                                <li key={number} className="page-num">
                                     <Link href={{
                                         pathname: `/catalog/${currentSlug}`,
                                         query: {
@@ -90,67 +92,81 @@ const Pagination = ({totalQuantityProducts, productsLength}) => {
                             )
                         }
                     }
-                    return null; // Важно: возвращаем null для пропущенных элементов
+                    return null
                 })}
-                {
-                    (currentPage < pageNumbers.length - 2 || currentPage == undefined) ?
-                        <li className="page-num"><span>...</span></li> : false
-                }
-                {(currentPage < pageNumbers.length - 1 || currentPage == undefined) ?
-                    <li className="page-num"><Link href={{
-                        pathname: `/catalog/${currentSlug}`,
-                        query: {
-                            ...routerQueries,
-                            page: pageNumbers.length
-                        }
-                    }}
-                    ><a>{pageNumbers.length}</a></Link></li>
-                    : false}
 
-                <li className={`page-next ${currentPage == Math.ceil(totalQuantityProducts / quantityProducts) || productsLength < quantityProducts ? 'not_active' : ''}`}>
-                    <Link href={{
-                        pathname: `/catalog/${currentSlug}`,
-                        query: {
-                            ...routerQueries,
-                            page: currentPage == undefined ? 2 : currentPage + 1
-                        }
-                    }}>
-                        <a>&nbsp;</a>
-                    </Link>
-                </li>
-            </ul>
-            <ul className="pagelist_mobile menu-default">
+                {/* Многоточие справа */}
+                {currentPage < totalPages - 2 && (
+                    <li className="page-num">
+                        <span>...</span>
+                    </li>
+                )}
 
-                <li className={`page-prev ${(currentPage == undefined || currentPage == 1) ? 'not_active' : ''}`}>
-                    {currentPage > 2 ?
+                {/* Последняя страница (если не первая) */}
+                {totalPages > 1 && (
+                    currentPage === totalPages ? (
+                        <li className="page-num active-num">
+                            <span>{totalPages}</span>
+                        </li>
+                    ) : (
+                        <li className="page-num">
+                            <Link href={{
+                                pathname: `/catalog/${currentSlug}`,
+                                query: {
+                                    ...routerQueries,
+                                    page: totalPages
+                                }
+                            }}>
+                                <a>{totalPages}</a>
+                            </Link>
+                        </li>
+                    )
+                )}
+
+                {/* Кнопка "Следующая" */}
+                <li className={`page-next ${currentPage === totalPages ? 'not_active' : ''}`}>
+                    {currentPage < totalPages && (
                         <Link href={{
                             pathname: `/catalog/${currentSlug}`,
                             query: {
+                                ...routerQueries,
+                                page: currentPage + 1
+                            }
+                        }}>
+                            <a>&nbsp;</a>
+                        </Link>
+                    )}
+                </li>
+            </ul>
+            
+            {/* Мобильная версия */}
+            <ul className="pagelist_mobile menu-default">
+                <li className={`page-prev ${currentPage === 1 ? 'not_active' : ''}`}>
+                    {currentPage > 1 && (
+                        <Link href={{
+                            pathname: `/catalog/${currentSlug}`,
+                            query: currentPage === 2 ? routerQueriesWithoutPage : {
                                 ...routerQueries,
                                 page: currentPage - 1
                             }
-                        }}
-                        ><a></a></Link>
-                        :
+                        }}>
+                            <a></a>
+                        </Link>
+                    )}
+                </li>
+                
+                <li className={`page-next ${currentPage === totalPages ? 'not_active' : ''}`}>
+                    {currentPage < totalPages && (
                         <Link href={{
                             pathname: `/catalog/${currentSlug}`,
                             query: {
                                 ...routerQueries,
+                                page: currentPage + 1
                             }
-                        }}
-                        ><a></a></Link>
-                    }
-                </li>
-                <li className={`page-next ${currentPage == Math.ceil(totalQuantityProducts / quantityProducts) || productsLength < quantityProducts ? 'not_active' : ''}`}>
-                    <Link href={{
-                        pathname: `/catalog/${currentSlug}`,
-                        query: {
-                            ...routerQueries,
-                            page: currentPage == undefined ? 2 : currentPage + 1
-                        }
-                    }}>
-                        <a>&nbsp;</a>
-                    </Link>
+                        }}>
+                            <a>&nbsp;</a>
+                        </Link>
+                    )}
                 </li>
             </ul>
         </div>
